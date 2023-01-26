c06606fd1d7b31afe4f2dacec76ca9055749638b
Camael Ventura <camael.ventura@gmail.com>
2023-01-26 01:44:17 -0300

    build

--- dist/index.js
+++ dist/index.js
@@ -9777,11 +9777,13 @@ const github = __nccwpck_require__(3218);
 
 try {
 
+  console.log('My test', github.context.payload.pull_request);
+
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   const time = (new Date()).toTimeString();
 
-  console.log(`Hello ${nameToGreet}!`);
+  // console.log(`Hello ${nameToGreet}!`);
 
   core.setOutput("time", time);
 
@@ -9789,7 +9791,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
 
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   
   core.setFailed(error.message);
