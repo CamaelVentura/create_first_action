c943ca5f3388856fccfc458b6d23085abaa824d7
Camael Ventura <camael.ventura@gmail.com>
2023-01-26 01:35:30 -0300

    logging

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const github = require('@actions/github');
 
 try {
 
-  console.log(github.context.payload.pull_request);
+  console.log('My test', github.context.payload.pull_request);
 
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   const time = (new Date()).toTimeString();
 
-  console.log(`Hello ${nameToGreet}!`);
+  // console.log(`Hello ${nameToGreet}!`);
 
   core.setOutput("time", time);
 
@@ -17,7 +17,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
 
-  console.log(`The event payload: ${payload}`);
+  //console.log(`The event payload: ${payload}`);
 } catch (error) {
   
   core.setFailed(error.message);
